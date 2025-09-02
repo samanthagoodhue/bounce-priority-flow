b32b9e96668ce0a6ccdf71842b882689260e27c5
samgdh <sam@apian.aero>
2025-09-02 22:17:13 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/letsbounce/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -21,3 +20,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
